642ed420c81256dba1b40c29085ae549e575fa36
Jean David AGUILAR <aguilar.jeandavid@yahoo.com>
2023-08-29 16:18:50 +0200

    add

--- main.c
+++ main.c
@@ -1,122 +1,43 @@
 #include <stdio.h>
-#include <limits.h>
-
+#include <stdlib.h>
+void mem(){
+int * memoirePerte;
+memoirePerte=malloc(sizeof(int));
+*memoirePerte=5;
+free(memoirePerte);
+}
 int main()
 {
-//int x = 3 ;
-//long unsigned m=18 ;
-//int d = 5;
-//int *v = &x;
-//int s1=sizeof(m);
-//int s2=sizeof(d);
-//int s3=sizeof(v);
-//printf ("i : %d\n",s1);
-//printf ("d : %d\n",s2);
-//printf ("v : %d\n",s3);
+    // Non init
+    int tmp=5;
+    printf("non init : %d\n",tmp);
+    int div0=3;
+    printf("non init : %d\n",div0);
+    // Perte memoire
+    mem();
 
-//int y;
-//for (y = 0; y < 6; y++) {
-//    int i;
-//    for (i = 0; i < 5; i++) {
-//        printf("i = %d\n",i);
-//        if (i == 3)
-//        break;
-//    }
-//    printf("y = %d\n",y);
+    //division par 0
+    int num = 10;
+    int denom = 0;
+    int result;
+    result = 1/denom;
+    result++;
+    printf("Result: %d\n", result);
 
-//}
-//int i = 4;
-//int j = 12;
-//int k = 5;
-//while(i){
-//        i-=1;
-//        printf ("i : %d\n",i);
-//    while(j){
-//            j-=1;
-//            printf ("j : %d\n",j);
-//        while(k){
-//                k-=1;
-//                printf ("k : %d\n",k);
-//                if (i==0 && j == 0 && k==0){
+    //modifie une variable par un pointeur qui déborde de sa valeur pointée
+    int a=3;
+    int b=5;
+    printf("Adresse de a : %p\n", &a);
+    printf("Adresse de b : %p\n", &b);
+    int *x=&a;
+    printf("Adresse de x : %p\n", x-1);
+    *(x-1)=9;
 
-//                goto end;}
-//        }
-//    }
-//}
-//end:
-//                printf("fini\n");
-//                printf ("i : %d\n",i);
-//                printf ("j : %d\n",j);
-//                printf ("k : %d\n",k);
-//    char lettre='a';
-//    while (1){
-
-
-//        if (lettre <='z'){
-//            printf ("lettre : %c\n",lettre);}
-//        else{break;}
-
-//        lettre++;
-
-//}
-//    char Lettre='A';
-//    while (1){
-
-
-//        if (Lettre <='z'){
-//            printf ("Lettre : %c\n",Lettre);}
-//        else{break;}
-
-//        Lettre++;
-
-//}
-//    int i = 23674; int j = -23674;
-//    long int k = (1l << 32);
-//    double x = 1e-8 + 1000;
-//    char c = 'A';
-//    char *chaine = "chaine de caracteres";
-//    printf("impression de i: \n");
-//    printf("%d \t %u \t %o \t %x",i,i,i,i);
-//    printf("\impression de j: \n");
-//    printf("%d \t %u \t %o \t %x",j,j,j,j);
-//    printf("\n%.2f \t %.2e",x,x);
-//    printf("\n%.20f \t %.20e",x,x);
-//    printf("\nimpression de c: \n");
-//    printf("%c \t %d",c,c);
-//    printf("\nimpression de chaine: %s\t %.10s \n",chaine,chaine);
-//    int EntreJoueur;
-//    int nombreOr = 10;
-
-//    scanf ("%d",&EntreJoueur);
-//    printf ("%d",EntreJoueur);
-
-    const int nombreOr = 42; // Le nombre d'or à deviner
-    int nombreSaisi, passes = 0;
-
-    printf("Bienvenue dans le jeu du nombre d'or!\n");
-    printf("Devinez le nombre d'or, il est compris entre 0 et 100.\n");
-
-    printf("Entrez un nombre : ");
-    scanf("%d", &nombreSaisi);
-
-    while (nombreSaisi != nombreOr) {
-        passes++; // Incrémentation du nombre de tentatives
-
-        if (nombreSaisi > nombreOr) {
-            printf("Le nombre d'or est plus petit.\n");
-        } else {
-            printf("Le nombre d'or est plus grand.\n");
-        }
-
-        printf("Entrez un nombre : ");
-        scanf("%d", &nombreSaisi);
+    printf("b : %d\n",b);
+    int k=0;
+    while(1){
+        k++;
+        printf ("coucou");
     }
-
-    printf("Bravo, vous avez trouvé le nombre d'or en %d passes!\n", passes);
-
     return 0;
 }
-
-
-
-
